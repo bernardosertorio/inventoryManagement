fc233d15548ce7e1aaf897e5caefc8bd4d7420ae
Bernardo Sertório <besertorio@gmail.com>
2021-02-26 20:06:25 -0300

    creating CreateUserService

---
+++ backend/src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,39 @@
+import { injectable, inject } from 'tsyringe';
+import { hash } from 'bcryptjs';
+
+import IUsersRepository from '../repositories/IUsersRepository';
+import User from '../infra/typeorm/entities/User';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+@injectable()
+class CreateUserService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  public async execute({ name, email, password }: IRequest): Promise<User> {
+    const checkUserExists = await this.usersRepository.findByEmail(email);
+
+    if (checkUserExists) {
+      throw new Error('Email address already used.');
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    const user = await this.usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return user;
+  }
+}
+
+export default CreateUserService;
