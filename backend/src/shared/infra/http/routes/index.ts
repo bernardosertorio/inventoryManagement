3b2cdb7606ee76d65dc7757d1e7478fde87a1223
Bernardo Sertório <besertorio@gmail.com>
2021-02-28 16:23:23 -0300

    extending product models in the container

--- backend/src/shared/infra/http/routes/index.ts
+++ backend/src/shared/infra/http/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import usersRouter from '../../../../modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '../../../../modules/users/infra/http/routes/sessions.routes';
 import categoryRouter from '../../../../modules/category/infra/http/routes/category.routes';
+import productRouter from '../../../../modules/product/infra/http/routes/product.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/category', categoryRouter);
+routes.use('/product', productRouter);
 
 export default routes;
