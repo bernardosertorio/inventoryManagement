5b8e41c0977e5cdf8378905a23beccc95866dc51
Bernardo Sertório <besertorio@gmail.com>
2021-02-28 16:11:49 -0300

    creating ProductController

---
+++ backend/src/modules/product/infra/http/controllers/ProductController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import CreateProductService from '../../../services/CreateProductService';
+
+export default class ProductController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { id, title, availability, description, price, sku } = request.body;
+
+    const createProduct = container.resolve(CreateProductService);
+
+    const product = await createProduct.execute({
+      id,
+      title,
+      availability,
+      description,
+      price,
+      sku,
+    });
+
+    return response.json(product);
+  }
+}
