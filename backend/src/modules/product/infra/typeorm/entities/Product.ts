4bc89e80824e8132cb6ebc1f63ce176c6bb58cea
Bernardo Sertório <besertorio@gmail.com>
2021-02-28 15:14:23 -0300

    creating Product entiti

---
+++ backend/src/modules/product/infra/typeorm/entities/Product.ts
@@ -0,0 +1,36 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  availability: boolean;
+
+  @Column()
+  description: string;
+
+  @Column()
+  price: number;
+
+  @Column()
+  sku: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Product;
