d97872e1f8f51ba2b52bf336d3f1e373199742f3
Bernardo Sertório <besertorio@gmail.com>
2021-03-02 17:24:50 -0300

    creating GetSkusInproductService

---
+++ backend/src/modules/products/services/GetSkusInProductService.ts
@@ -0,0 +1,26 @@
+import { injectable, inject } from 'tsyringe';
+
+import Sku from '../infra/typeorm/entities/Sku';
+import ISkuRepository from '../repositories/ISkuRepository';
+
+interface IRequest {
+  product_id: string;
+}
+
+@injectable()
+class GetSkusInProductService {
+  constructor(
+    @inject('SkuRepository')
+    private skuRepository: ISkuRepository,
+  ) {}
+
+  public async execute({ product_id }: IRequest): Promise<Sku[]> {
+    const skusInCategory = await this.skuRepository.findAllSkusInProduct({
+      product_id,
+    });
+
+    return skusInCategory;
+  }
+}
+
+export default GetSkusInProductService;
