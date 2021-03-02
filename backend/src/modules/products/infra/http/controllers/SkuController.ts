cc4488e4b1a909050b43396a0d5b59a7c2f9c45b
Bernardo Sertório <besertorio@gmail.com>
2021-03-02 14:07:35 -0300

    creating SkuController.ts

---
+++ backend/src/modules/products/infra/http/controllers/SkuController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import CreateSkuService from '../../../services/CreateSkuService';
+
+export default class SkuController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { product_id } = request.params;
+    const { title, amount, sizes, colors, materials } = request.body;
+
+    const createSku = container.resolve(CreateSkuService);
+
+    const sku = await createSku.execute({
+      title,
+      amount,
+      sizes,
+      colors,
+      materials,
+      product_id,
+    });
+
+    return response.json(sku);
+  }
+}
