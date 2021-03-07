bf5904e2f35dff3965cc7bd4912e5f53fb740449
Bernardo Sertório <besertorio@gmail.com>
2021-03-07 19:20:59 -0300

    creating routes shared

--- backend/src/shared/infra/http/routes/index.ts
+++ backend/src/shared/infra/http/routes/index.ts
@@ -1,17 +1,22 @@
 import { Router } from 'express';
+import usersRoutes from '../../../../modules/users/infra/http/routes/users.routes';
+import sessionsRoutes from '../../../../modules/users/infra/http/routes/sessions.routes';
 
-import usersRouter from '../../../../modules/users/infra/http/routes/users.routes';
-import sessionsRouter from '../../../../modules/users/infra/http/routes/sessions.routes';
-import categoryRouter from '../../../../modules/products/infra/http/routes/category.routes';
-import productRouter from '../../../../modules/products/infra/http/routes/product.routes';
-import skuRouter from '../../../../modules/products/infra/http/routes/sku.routes';
+import categoryRoutes from '../../../../modules/products/infra/http/routes/category.routes';
+import colorRoutes from '../../../../modules/products/infra/http/routes/color.routes';
+import modelRoutes from '../../../../modules/products/infra/http/routes/model.routes';
+import sizeRoutes from '../../../../modules/products/infra/http/routes/size.routes';
+import productRoutes from '../../../../modules/products/infra/http/routes/product.routes';
 
 const routes = Router();
 
-routes.use('/users', usersRouter);
-routes.use('/sessions', sessionsRouter);
-routes.use('/categories', categoryRouter);
-routes.use('/products', productRouter);
-routes.use('/skus', skuRouter);
+routes.use('/users', usersRoutes);
+routes.use('/sessions', sessionsRoutes);
+
+routes.use('/categories', categoryRoutes);
+routes.use('/colors', colorRoutes);
+routes.use('/models', modelRoutes);
+routes.use('/sizes', sizeRoutes);
+routes.use('/products', productRoutes);
 
 export default routes;
