99baa1e03e5a21744f439c7669c577e631f30b14
Bernardo Sertório <besertorio@gmail.com>
2021-03-07 09:38:26 -0300

    creating colors table

---
+++ backend/src/shared/infra/typeorm/migrations/1615120608118-CreateColors.ts
@@ -0,0 +1,38 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateColors1615120608118 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'colors',
+        columns: [
+          {
+            name: 'code',
+            type: 'varchar',
+            isPrimary: true,
+            isUnique: true,
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('colors');
+  }
+}
